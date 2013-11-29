6d68f1ff5966ab509a1e572616f75ea18b4442b9
Cyril Margorin <comargo@gmail.com>
2013-11-29 17:56:16 +0400

    Use standard web reference (override in separate file)

---
+++ AnkhMantisConnector/IssueTracker/org.mantis.www.cs
@@ -0,0 +1,21 @@
+﻿namespace AnkhMantisConnector.org.mantisbt.www
+{
+    public partial class MantisConnect
+    {
+        private Ookii.Dialogs.CredentialDialog credDlg = null;
+        public MantisConnect(string url)
+        {
+            this.Url = url;
+            if ((this.IsLocalFileSystemWebService(this.Url) == true))
+            {
+                this.UseDefaultCredentials = true;
+                this.useDefaultCredentialsSetExplicitly = false;
+            }
+            else
+            {
+                this.useDefaultCredentialsSetExplicitly = true;
+            }
+        }
+
+
+}
