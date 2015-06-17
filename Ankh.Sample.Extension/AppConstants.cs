b5bd944717a7de673f9539d922c46107db4fb99b
Ambroise Petitgenêt <apetitgenet@free.fr>
2015-06-17 14:10:06 +0200

    Comprendre pourquoi quand on change le nom on a plus le format plugin mais dll

---
+++ Ankh.Sample.Extension/AppConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ankh.Sample.Extension
+{
+    static class AppConstants
+	{
+		public const string PackageGUID = GuidList.guidAnkhSampleExtensionPkgString;
+		public const string ServiceGUID = GuidList.guidAnkhSampleExtensionConnectorString;
+
+		public const string CompanyName = "AnkhSVN";
+        public const string PackageName = "AnkhSVN Sample Extension";
+		public const string ConnectorName = "AnkSVN Sample Issue Repository Connector";
+    };
+}
